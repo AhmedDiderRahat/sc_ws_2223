36ad11d241e5f67fd63f374a7349073ecba2bec6
ahmeddiderrahat <adrahat.bht@gmail.com>
2022-10-07 11:50:04 +0200

    Topic 4- Operators and Vectorized Operations

---
+++ Course Material/R_code/T4_Operators_Vectorized_Operations.R
@@ -0,0 +1,24 @@
+x <- 1:4  # 1, 2, 3, 4
+
+y <- 6:9  # 6, 7, 8, 9
+
+x+y       # element-wise sum
+
+x > 2     # where, x is greater than 2
+
+x >= 2    # where, x is greater than or equal to 2
+
+y == 8    # where, y is 8
+
+x / y     # vector divison 
+
+# -------------------------------- x --------------------------------
+
+# Operators
+
+x <- c(TRUE, TRUE)
+y <- c(TRUE, FALSE)
+
+print(x & y)  # element-wise AND operation
+
+print(x && y) # Logical AND
