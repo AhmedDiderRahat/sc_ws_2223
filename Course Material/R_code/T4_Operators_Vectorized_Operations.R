0f223d6f1b4be684b3f854685eadc7df2110bfe0
ahmeddiderrahat <adrahat.bht@gmail.com>
2022-12-08 14:47:12 +0100

    revised lecture 5

--- Course Material/R_code/T4_Operators_Vectorized_Operations.R
+++ Course Material/R_code/T4_Operators_Vectorized_Operations.R
@@ -22,3 +22,10 @@ y <- c(TRUE, FALSE)
 print(x & y)  # element-wise AND operation
 
 print(x && y) # Logical AND
+
+# TRUE
+# Warning messages:
+# 1: In x && y : 'length(x) = 2 > 1' in coercion to 'logical(1)'
+# 2: In x && y : 'length(x) = 2 > 1' in coercion to 'logical(1)'
+
+# Output is TRUE, but we need to apply logical into two object with length
